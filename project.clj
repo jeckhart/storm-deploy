fce5a00c3cd1a3a644ca08addfb23ac7f308ea80
John Eckhart <jeckhart@yodle.com>
2015-05-18 23:48:12 -0400

    Update jclouds

--- project.clj
+++ project.clj
@@ -1,7 +1,13 @@
 (defproject storm-deploy "0.0.6-SNAPSHOT"
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
-  :profiles {:dev {:resource-paths ["conf"]}}
+  :profiles {
+    :dev {
+      :resource-paths ["conf"]
+      :plugins [[com.palletops/pallet-lein "0.8.0-alpha.1"]]
+    }
+    :pallet {:dependencies [[com.palletops/pallet "0.8.0-RC.11"]]}
+  }
   :aliases {"deploy-storm" ["run" "-m" "backtype.storm.provision"]}
 
   :repositories {
@@ -10,19 +16,8 @@
                  }
 
   :dependencies [
-                 [storm "0.5.4"]
+                 [org.apache.storm/storm-core "0.9.4"]
                  [commons-codec "1.4"]
-                 [org.cloudhoist/pallet "0.7.3"]
-                 [org.cloudhoist/pallet-jclouds "1.5.1"]
-                 [org.cloudhoist/java "0.5.0"]
-                 [org.cloudhoist/git "0.5.0"]
-                 [org.cloudhoist/ssh-key "0.5.0"]
-                 [org.cloudhoist/automated-admin-user "0.5.0"]
-                 [org.cloudhoist/iptables "0.5.0"]
-                 [org.cloudhoist/maven "0.5.0"]
-                 [org.cloudhoist/zookeeper "0.5.1"]
-                 [org.cloudhoist/nagios-config "0.5.0"]
-                 [org.cloudhoist/crontab "0.5.0"]
 
                  [com.jcraft/jsch.agentproxy.usocket-jna "0.0.5"]
                  [com.jcraft/jsch.agentproxy.usocket-nc "0.0.5"]
@@ -31,13 +26,13 @@
                  [com.jcraft/jsch.agentproxy.core "0.0.5"]
                  [com.jcraft/jsch.agentproxy.jsch "0.0.5"]
                  [com.jcraft/jsch "0.1.49"]
-                 [org.jclouds.driver/jclouds-sshj "1.5.2"]
-                 [org.jclouds.provider/aws-ec2 "1.5.2"]
-                 [org.jclouds.provider/aws-s3 "1.5.2"]
+                 [org.apache.jclouds.driver/jclouds-sshj "1.7.3"]
+                 [org.apache.jclouds.provider/aws-ec2 "1.7.3"]
+                 [org.apache.jclouds.provider/aws-s3 "1.7.3"]
 
                  [log4j/log4j "1.2.14"]
                  [jvyaml "1.0.0"]]
 
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [org.cloudhoist/pallet-lein "0.5.2"]]
+;  :dev-dependencies [[swank-clojure "1.3.0"]
+;                     [org.cloudhoist/pallet-lein "0.5.2"]]
   :min-lein-version "2.0.0")
